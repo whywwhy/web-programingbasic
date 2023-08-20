711a4a19c69615f617d59ac1b3926fe8582a3f0f
je355 <120763503+je355@users.noreply.github.com>
2023-08-20 20:59:43 +0900

    webprograming
    Web Class for the 1st Semester 2023

---
+++ web-school/html/proj1/me.js
@@ -0,0 +1,12 @@
+function mJava(pJob){
+    if(pJob==1)
+        document.getElementById("idH1").innerHTML="메롱";
+    else if(pJob==2)
+        document.getElementById("idH5").style="color:green";
+    else if(pJob==3)
+        document.getElementById("idIM").src="http://arabbit.link/image/tiger.png";
+    else if(pJob==4)
+        document.getElementById("idH3").style="display:none";
+    else if(pJob==5)
+        document.getElementById("idH3").style="display:block";
+}
